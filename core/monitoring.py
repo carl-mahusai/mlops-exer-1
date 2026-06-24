e00cc0fd9425c1d21a0d8999940f5bad9771970e
Carl Manalo <clmanalo@mahusai.ph>
2026-06-25 06:12:29 +0800

    Correct setup of evidently logs

--- core/monitoring.py
+++ core/monitoring.py
@@ -41,10 +41,6 @@ def generate_evidently_report(dm, model, mlflow_logger, args):
             y.cpu().numpy().astype(int)
         )
 
-        # scores.extend(
-        #     probs.cpu().numpy()
-        # )
-
         scores.extend(
             probs.cpu().numpy().flatten().tolist()
         )
@@ -54,16 +50,6 @@ def generate_evidently_report(dm, model, mlflow_logger, args):
         "prediction": predictions,
         "score": scores
     })
-
-    # dataset = Dataset.from_pandas(
-    #     eval_df,
-    #     data_definition=DataDefinition(
-    #         classification={
-    #             "target": "target",
-    #             "prediction_labels": "prediction"
-    #         }
-    #     )
-    # )
 
     dataset = Dataset.from_pandas(
         eval_df,
@@ -82,26 +68,28 @@ def generate_evidently_report(dm, model, mlflow_logger, args):
         metrics=[ClassificationPreset()]
     )
 
-    script_dir = Path(__file__).resolve().parent.parent
-    reports_dir = script_dir / "reports"
 
-    reports_dir = Path(reports_dir)
-    reports_dir.mkdir(exist_ok=True)
-
-    report_path = reports_dir / "classification_report.html"
-
-    snapshot = report.run(dataset)
-
-    snapshot.save_html(str(report_path))
 
     if mlflow_logger:
+
+        script_dir = Path(__file__).resolve().parent.parent
+        # reports_dir = script_dir / "reports"
+
+        # run_report_dir = reports_dir / mlflow_logger.run_id
+
+        # run_report_dir = Path(run_report_dir)
+        run_report_dir = script_dir / "reports" / mlflow_logger.run_id
+        run_report_dir.mkdir(parents=True, exist_ok=True)
+
+
+        report_path = run_report_dir / "classification_report.html"
+
+        snapshot = report.run(dataset)
+
+        snapshot.save_html(str(report_path))
+
         with mlflow.start_run(run_id=mlflow_logger.run_id):
             mlflow.log_artifact(
                 str(report_path),
                 artifact_path="reports"
             )
-
-            # mlflow.log_artifact(
-            #     str(json_path),
-            #     artifact_path="reports"
-            # )
